4def1e6b1bdb208741b71bb741cae120a5cc514f
Trinhf <lnktrinh9.2@gmail.com>
2026-03-16 02:30:06 +0700

    add new classes

---
+++ Build_class/Build_class/Advertisement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Project_OOP
+{
+    public class Advertisement
+    {
+        private string _sponsorName;
+        private string _content;
+
+        public string SponsorName
+        {
+            get { return _sponsorName; }
+            set { _sponsorName = value; }
+        }
+
+        public string Content
+        {
+            get { return _content; }
+            set { _content = value; }
+        }
+
+        public Advertisement()
+        {
+        }
+
+        public Advertisement(string sponsorName, string content)
+        {
+            _sponsorName = sponsorName;
+            _content = content;
+        }
+
+        public string ShowAd()
+        {
+            string result = "=== Advertisement ===\n";
+            result = result + "Sponsor: " + _sponsorName + "\n";
+            result = result + "Content: " + _content + "\n";
+            result = result + "=====================";
+            return result;
+        }
+    }
+}
---
+++ Build_class/Build_class/Tag.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Project_OOP
+{
+    public class Tag
+    {
+        private string _tagName;
+        private int _clickCount;
+
+        public string TagName
+        {
+            get { return _tagName; }
+            set { _tagName = value; }
+        }
+
+        public int ClickCount
+        {
+            get { return _clickCount; }
+            set { _clickCount = value; }
+        }
+
+        public Tag()
+        {
+            _clickCount = 0;
+        }
+
+        public Tag(string tagName)
+        {
+            _tagName = tagName;
+            _clickCount = 0;
+        }
+
+        public void Click()
+        {
+            _clickCount = _clickCount + 1;
+        }
+    }
+}
