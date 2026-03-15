f13bbd4eac5fd98c9a5b9248dd40ff3184a7f54b
Trinhf <lnktrinh9.2@gmail.com>
2026-03-16 02:30:06 +0700

    add new classes

---
+++ Build_class/Build_class/Category.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project_OOP
+{
+    public class Category
+    {
+        private int _id;
+        private string _name;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Category()
+        {
+        }
+
+        public Category(int id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+    }
+}
---
+++ Build_class/Build_class/Comment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Project_OOP
+{
+    [Serializable]
+    public class Comment
+    {
+        private string _content;
+        private Person _author;
+        private DateTime _timestamp;
+
+        public string Content
+        {
+            get { return this._content; }
+            set { this._content = value; }
+        }
+
+        public Person Author
+        {
+            get { return this._author; }
+            set { this._author = value; }
+        }
+
+        public DateTime Timestamp
+        {
+            get { return this._timestamp; }
+            set { this._timestamp = value; }
+        }
+
+        // Constructor không tham số (phục vụ serialize)
+        public Comment()
+        {
+            this._timestamp = DateTime.Now;
+        }
+
+        // Constructor đầy đủ
+        public Comment(string content, Person author)
+        {
+            this._content = content;
+            this._author = author;
+            this._timestamp = DateTime.Now;
+        }
+    }
+}
