3707be9ca7054fc42f84faa3a0d0a85127be8a8e
Ngoc <Baongoc29082006@gmail.com>
2026-03-16 15:57:24 +0700

    add classes

---
+++ Project_OOP/BreakingNews.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_OOP
+{
+    [Serializable]
+    public class BreakingNews : DailyNews
+    {
+        private bool _isConfirmed;
+
+        public BreakingNews() : base() { }
+
+        public BreakingNews(string id, string title, DateTime publishDate, int trendLevel)
+            : base(id, title, publishDate, trendLevel)
+        {
+            this._isConfirmed = false;
+        }
+
+        public void Confirm()
+        {
+            _isConfirmed = true;
+        }
+
+        public override void DisplayContent()
+        {
+            Console.WriteLine("********************");
+            Console.WriteLine("TIN KHẨN CẤP");
+            if (_isConfirmed == true)
+            {
+                Console.WriteLine("Trạng thái: Đã xác thực");
+            }
+
+            // tái sử dụng logic của lớp cha
+            base.DisplayContent();
+        }
+    }
+}
---
+++ Project_OOP/Publication .cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_OOP
+{
+    [Serializable]
+    public abstract class Publication
+    {
+        protected string _id;
+        protected string _title;
+        protected DateTime _publishDate;
+        protected int _viewCount = 0;
+
+        public Publication() { }
+
+        public Publication(string id, string title, DateTime publishDate)
+        {
+            this._id = id;
+            this._title = title;
+            this._publishDate = publishDate;
+        }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        public DateTime PublishDate
+        {
+            get { return _publishDate; }
+            set { _publishDate = value; }
+        }
+
+        public int ViewCount
+        {
+            get { return _viewCount; }
+        }
+
+        public void IncrementView()
+        {
+            _viewCount = _viewCount + 1;
+        }
+
+        public abstract void DisplayContent();
+    }
+}
