95ccd26159868bddbb0bea6e7942fd4d9e68bbe9
haiho2365 <haiho2365@gmail.com>
2026-03-17 00:27:56 +0700

    Update

---
+++ Project_OOP/Service/EmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Project_OOP
+{
+    public class EmailService : INotificationService
+    {
+        public void SendNotification(Person recipient, string message)
+        {
+            string userEmail = recipient.Email;
+
+            string formattedMessage = "[Email] Đang gửi thư tới: " + userEmail + " | Nội dung: " + message;
+            Console.WriteLine(formattedMessage);
+        }
+    }
+}
---
+++ Project_OOP/Service/PushNotificationService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Project_OOP
+{
+    public class PushNotificationService : INotificationService
+    {
+        public void SendNotification(Person recipient, string message)
+        {
+            string userEmail = recipient.Email;
+
+            string formattedMessage = "[Popup] Hiển thị trên màn hình của: " + userEmail + " | Nội dung: " + message;
+            Console.WriteLine(formattedMessage);
+        }
+    }
+}
