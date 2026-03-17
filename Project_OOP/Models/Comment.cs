766ca9739aac530a48bf667dff559613f7fbe978
lnktrinh92-boom <lnktrinh9.2@gmail.com>
2026-03-17 14:53:17 +0700

    Update Comment.cs

--- Project_OOP/Models/Comment.cs
+++ Project_OOP/Models/Comment.cs
@@ -5,13 +5,13 @@ namespace Project_OOP
     [Serializable]
     public class Comment
     {
-        public string Content { get; set; }
-        public Person Author { get; set; }
-        public DateTime Timestamp { get; set; }
-
         private string _content;
         private Person _author;
         private DateTime _timestamp;
+
+        public string Content { get; set; }
+        public Person Author { get; set; }
+        public DateTime Timestamp { get; set; }
 
         public Comment()
         {
