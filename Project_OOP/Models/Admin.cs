094535e0ce21a80b48f9eee5492df6478c412b37
baovacai-beep <baovacai@gmail.com>
2026-03-18 00:30:31 +0700

    Update Admin.cs

--- Project_OOP/Models/Admin.cs
+++ Project_OOP/Models/Admin.cs
@@ -8,17 +8,6 @@ namespace Project_OOP
     {
         private string _password;
         private string _role;
-        public string Password
-        {
-            get { return _password; }
-            set { _password = value; }
-        }
-
-        public string Role
-        {
-            get { return _role; }
-            set { _role = value; }
-        }
         public Admin() : base() 
         { 
         }
@@ -28,17 +17,33 @@ namespace Project_OOP
             this._password = password;
             this._role = role;
         }
+        public string Password
+        {
+            get { return this._password; }
+            set { this._password = value; }
+        }
+
+        public string Role
+        {
+            get { return this._role; }
+            set { this._role = value; }
+        }
         public bool CheckPassword(string inputPass)
         {
-            return this._password == inputPass;
+            bool isCorrect = false;
+            if (this._password == inputPass)
+            {
+                isCorrect = true;
+            }
+            return isCorrect;
         }
         public override void ShowInfo()
         {
             Console.WriteLine("[QUẢN TRỊ VIÊN]");
-            Console.WriteLine("ID: " + this.Id); // Dùng Property Id từ lớp cha
+            Console.WriteLine("ID: " + this.Id); 
             Console.WriteLine("Họ tên: " + this.FullName);
             Console.WriteLine("Email: " + this.Email);
-            Console.WriteLine("Vai trò: " + this._role);
+            Console.WriteLine("Vai trò: " + this.Role);
             Console.WriteLine("-------------------------");
         }
     }
