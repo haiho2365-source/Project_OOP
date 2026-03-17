61d02fd2479a0c3b3768554ab7f9057e72b56647
baovacai-beep <baovacai@gmail.com>
2026-03-17 15:09:21 +0700

    Update Admin.cs
    Update theo yêu cầu của Hải và Thầy

--- Project_OOP/Models/Admin.cs
+++ Project_OOP/Models/Admin.cs
@@ -8,27 +8,36 @@ namespace Project_OOP
     {
         private string _password;
         private string _role;
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
 
-        public Admin() : base() { }
-
+        public string Role
+        {
+            get { return _role; }
+            set { _role = value; }
+        }
+        public Admin() : base() 
+        { 
+        }
         public Admin(string id, string fullName, string email, string password, string role)
             : base(id, fullName, email)
         {
             this._password = password;
             this._role = role;
         }
-
         public bool CheckPassword(string inputPass)
         {
             return this._password == inputPass;
         }
-
         public override void ShowInfo()
         {
             Console.WriteLine("[QUẢN TRỊ VIÊN]");
-            Console.WriteLine("ID: " + this._id);
-            Console.WriteLine("Họ tên: " + this._fullName);
-            Console.WriteLine("Email: " + this._email);
+            Console.WriteLine("ID: " + this.Id); // Dùng Property Id từ lớp cha
+            Console.WriteLine("Họ tên: " + this.FullName);
+            Console.WriteLine("Email: " + this.Email);
             Console.WriteLine("Vai trò: " + this._role);
             Console.WriteLine("-------------------------");
         }
