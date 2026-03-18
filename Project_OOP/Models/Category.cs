6bb8bfe13910ab8d0f3ac7feb1823dd7152e2aa7
lnktrinh92-boom <lnktrinh9.2@gmail.com>
2026-03-18 21:20:35 +0700

    Update Category.cs

--- Project_OOP/Models/Category.cs
+++ Project_OOP/Models/Category.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Project_OOP
 {
@@ -7,8 +7,17 @@ namespace Project_OOP
         private int _id;
         private string _name;
 
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
 
         public Category()
         {
@@ -16,8 +25,8 @@ namespace Project_OOP
 
         public Category(int id, string name)
         {
-            Id = id;
-            Name = name;
+            _id = id;
+            _name = name;
         }
     }
 }
