fcea9e538899df86e7884e86cfee491a666e331f
Trinhf <lnktrinh9.2@gmail.com>
2026-03-29 20:36:17 +0700

    Add classes

---
+++ Project_OOP/Database.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project_OOP
+{
+    [Serializable]
+    public class Database
+    {
+        private List<Subscriber> _subscribers;
+        private List<Admin> _admins;
+
+        public Database()
+        {
+            this._subscribers = new List<Subscriber>();
+            this._admins = new List<Admin>();
+        }
+
+        public List<Subscriber> Subscribers
+        {
+            get { return this._subscribers; }
+        }
+
+        public List<Admin> Admins
+        {
+            get { return this._admins; }
+        }
+
+        public void AddSubscriber(Subscriber subscriber)
+        {
+            this._subscribers.Add(subscriber);
+        }
+
+        public bool IsEmailExists(string email)
+        {
+            int i;
+            for (i = 0; i < this._subscribers.Count; i = i + 1)
+            {
+                if (this._subscribers[i].Email == email)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
