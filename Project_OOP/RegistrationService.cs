5fd56b2ea728b8e4a5098f5b6693e853d35acad8
Trinhf <lnktrinh9.2@gmail.com>
2026-03-29 20:36:17 +0700

    Add classes

---
+++ Project_OOP/RegistrationService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Project_OOP
+{
+    public class RegistrationService
+    {
+        private Database _database;
+
+        public RegistrationService(Database database)
+        {
+            this._database = database;
+        }
+
+        public bool Register(string id, string fullName, string email)
+        {
+            if (this._database.IsEmailExists(email) == true)
+            {
+                return false;
+            }
+
+            Subscriber newUser = new Subscriber(id, fullName, email, false);
+            this._database.AddSubscriber(newUser);
+
+            return true;
+        }
+    }
+}
